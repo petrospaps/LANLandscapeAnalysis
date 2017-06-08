2fd13b0671fd966e807de9ebe570ef8ec03877da
amberbu <bu.amber@gmail.com>
2017-06-08 14:06:59 +0100

    count pois of diff types, and output to json

--- POIExtractor/poi_meter.py
+++ POIExtractor/poi_meter.py
@@ -70,3 +70,30 @@ def poi_metric(lat,lon,sqdistance):
 
 pois = poi_metric(50.8996 , -1.4037, 0.5)
 print(pois)
+
+distance = 0.3
+lat = 50.8996
+lon = -1.4037
+
+poly = get_polygon(lat,lon,distance)
+pois_CnE = query_spoi(poly)
+pois_H = query_lgd(poly)
+pois = pois_H.union(pois_CnE)
+
+
+
+pois_dict ={}
+pois_dict["lat"] = lat
+pois_dict["lon"] = lon
+pois_dict["number of poi"] = len(pois)
+pois_dict["number of Culture_and_Entertainment places"] = len(pois_CnE)
+pois_dict["number of HistoricThing places"] = len(pois_H)
+pois_dict["Culture_and_Entertainment"] = list(pois_CnE)
+pois_dict["HistoricThing"] = list(pois_H)
+
+print pois_dict
+
+#json_str = json.dumps(pois_dict)
+
+with open('data.json', 'a') as f:
+    json.dump(pois_dict, f)
