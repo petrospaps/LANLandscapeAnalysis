a95613b9eab9b82b5e2663322924751566a11542
Qiong Bu <qb1g13@soton.ac.uk>
2017-06-09 12:23:59 +0100

    iterate given locations

--- POIExtractor/poi_meter.py
+++ POIExtractor/poi_meter.py
@@ -1,3 +1,4 @@
+import json
 import square_gen as sg
 import rdflib as rdf
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -71,27 +72,38 @@ def poi_metric(lat,lon,sqdistance):
 #pois = poi_metric(50.8996 , -1.4037, 0.5)
 #print(pois)
 
-distance = 0.3
+
+six_stories_soton = {"822583e8072c4f66655ab49b":(50.89967,-1.403664), "53dca9ee617b5965064e6833" :(50.902688,-1.404179),"6b3251d7db159be5dbfb637a":(50.899563,-1.405402),"b97e8521dc16ccee9580ad1d":(50.910578,-1.405019),"b53ec06f925d766c2415d510":(50.897369,-1.407312),"ab349646e7cc91ef38aa59cd":(50.902705,-1.411153),"e12ea776998b31b723712453":(50.896933,-1.394094)}
+
+distance = 0.5
+
+
+'''
 lat = 50.8996
 lon = -1.4037
-
 #poly = get_polygon(lat,lon,distance)
 #pois_CnE = query_spoi(poly)
 #pois_H = query_lgd(poly)
 #pois = pois_H.union(pois_CnE)
 pois = poi_metric(lat,lon,distance)
-
+'''
 
 
 pois_dict ={}
-pois_dict["lat"] = lat
-pois_dict["lon"] = lon
-pois_dict["number of poi"] = len(pois)
-pois_dict["points of interet"] = list(pois)
-#pois_dict["number of Culture_and_Entertainment places"] = len(pois_CnE)
-#pois_dict["number of HistoricThing places"] = len(pois_H)
-#pois_dict["Culture_and_Entertainment"] = list(pois_CnE)
-#pois_dict["HistoricThing"] = list(pois_H)
+for item in six_stories_soton:
+    #print item
+    lat = six_stories_soton[item][0]
+    lon = six_stories_soton[item][1]
+    pois_dict["location_id"] = item
+    #pois_dict["lat"] = lat
+    #pois_dict["lon"] = lon
+    pois = poi_metric(lat, lon, distance)
+    pois_dict["number of poi"] = len(pois)
+    pois_dict["points of interet"] = list(pois)
+    #pois_dict["number of Culture_and_Entertainment places"] = len(pois_CnE)
+    #pois_dict["number of HistoricThing places"] = len(pois_H)
+    #pois_dict["Culture_and_Entertainment"] = list(pois_CnE)
+    #pois_dict["HistoricThing"] = list(pois_H)
 
 print pois_dict
 
